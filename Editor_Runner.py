92aeb50b5f1f37f943e749c806dc40e0b4388a4a
MargowoN2705 <margowon2705@gmail.com>
2025-06-22 20:32:57 +0200

    Created New Maps

--- Editor_Runner.py
+++ Editor_Runner.py
@@ -56,7 +56,7 @@ def main():
     screen_info = Info()
 
     editor = MapEditor(
-        map_path = "game_map/maps_storage/map_13.map",
+        map_path = "game_map/maps_storage/map_23.map",
 
         tile_image_data=tile_image_data,
         tile_size=32,
